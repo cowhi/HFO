1d2cdf1afe9fc59fe0f0475d900d55df2908b0c1
cowhi <github.rg@gmail.com>
2016-09-09 21:42:38 -0300

    Dummy and evaluation update

--- experiments/agents/dummy.py
+++ experiments/agents/dummy.py
@@ -5,11 +5,11 @@ from .agent import Agent
 
 class Dummy(Agent):
 
-    def __init__(self):
-        super(Dummy, self).__init__()
+    def __init__(self, seed):
+        super(Dummy, self).__init__(seed)
 
 
-    def select_action(self,state):
+    def select_action(self,state, stateFeatures):
         """ When this method is called, the agent executes an action. """
         if state[5] == 1: # State[5] is 1 when the player can kick the ball
             #return random.choice([SHOOT, PASS(team_mate), DRIBBLE])
@@ -28,5 +28,5 @@ class Dummy(Agent):
         status = self.hfo.step()
         statePrime = self.get_transformed_features(self.hfo.getState())
         reward = self.get_reward(status)
-        actionPrime = self.select_action(statePrime)
+        actionPrime = self.select_action(statePrime, statePrime)
         return status, statePrime, actionPrime
--- experiments/drawing_util.py
+++ experiments/drawing_util.py
@@ -1,4 +1,5 @@
 import numpy as np
 
 def get_data_from_file(filename):
-    data = np.loadtxt(filename, skiprows=1, delimiter=",", usecols=range(1,5), unpack=True)
+    data = np.loadtxt(filename, skiprows=1, delimiter=",", usecols=(0,1,2,3))
+    return data
