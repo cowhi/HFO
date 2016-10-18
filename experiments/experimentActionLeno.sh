6588815c91ef69531fadba472f76a1c8d5fa4995
leno <f.leno@hotmail.com.br>
2016-10-18 08:18:43 -0200

    Preparing experiments

--- experiments/experimentActionLeno.sh
+++ experiments/experimentActionLeno.sh
@@ -1,7 +1,7 @@
-pkill -f  sh\ ru* 
-pkill -f python\ exp*
-pkill -f python\ /home/leno/HFO*
-killall -9 rcssserver
+#pkill -f  sh\ ru* 
+#pkill -f python\ exp*
+#pkill -f python\ /home/leno/HFO*
+#killall -9 rcssserver
 
 sh runSimpleExpServer.sh 22445 35 > log/serverAdHocVisitAction.log &
 sleep 5
@@ -11,8 +11,8 @@ sh runSimpleExpServer.sh 22945 33 > log/serverAdHocTDAction.log &
 sleep 5
 sh runSimpleExpAgent.sh 22945 AdHocTDAction 18 50 > log/logAdHocTDAction.log &
 
-sh runSimpleExpServer.sh 22645 32 > log/serverTorreyAction.log &
-sleep 5
-sh runSimpleExpAgent.sh 22645 TorreyAction 19 50 > log/logTorreyAction.log &
+#sh runSimpleExpServer.sh 22645 32 > log/serverTorreyAction.log &
+#sleep 5
+#sh runSimpleExpAgent.sh 22645 TorreyAction 19 50 > log/logTorreyAction.log &
 
 
